c40c515b758a0da8228e32f766855b3cc002f51c
aristovz <pavelaristov@MacBook-Air-Pavel.local>
2017-01-03 14:58:33 +0300

    Initial Commit

---
+++ VKMusicNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VKMusicNew
+//
+//  Created by Pavel Aristov on 03.01.17.
+//  Copyright © 2017 NetSharks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
